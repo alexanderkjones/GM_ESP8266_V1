eede0019253d33efc9129b379b0dfdbea8eec6ac
Jacob Johnson <jljtgr@jljtgr.com>
2016-01-27 15:22:27 -0500

    Initial Setup

---
+++ GM_ESP8266_V1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GM_ESP8266_V1.ino
+ Created:	1/27/2016 2:53:48 PM
+ Author:	Jacob
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
